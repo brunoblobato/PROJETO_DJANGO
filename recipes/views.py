fb0fac289ec81288aad1d605a80f61ee2e31038b
brunoblobato <brunoblobato@gmail.com>
2025-03-27 20:51:16 -0300

    Include data fake

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Bruno',
+        'recipes': [make_recipe() for _ in range(12)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Bruno',
+        'recipe': make_recipe(),
     })
